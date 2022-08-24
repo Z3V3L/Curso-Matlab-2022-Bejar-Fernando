73550fc3bac6383e66970aaee7bcf57a448b39db
Z3V3L <zevelbs@gmail.com>
2022-08-24 12:31:11 -0500

    Add files via upload

--- script_formula_1.m
+++ script_formula_1.m
@@ -3,7 +3,7 @@ clear all       %Clear variables
 close all       %Close windows
 
 a = 1;
-b = input("Dame B: ");
+b = input("Si tenemos x1 = (-b+(b^2-4ac)^(1/2))/(2a)  y  x2 = (-b-(b^2-4ac)^(1/2))/(2a)\nDame B: ");
 c = input("Dame C: ");
 x1 = (-b+(b^2-4*a*c)^(1/2))/(2*a);
 x2 = (-b-(b^2-4*a*c)^(1/2))/(2*a);
